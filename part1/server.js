26b806ecc967bad38c40cf14c1ba130254f9a0d5
NickDErrico <nickderrico@hotmail.com>
2018-01-05 11:49:13 -0700

    part 1 finished

--- part1/server.js
+++ part1/server.js
@@ -2,9 +2,29 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.get('/hello', function(req, res) {
+  res.send("hello");
 });
+
+app.post('/create/:name', function(req, res) {
+  let nameObj = {
+    id: 1,
+    name: req.params.name
+  }
+  res.json(nameObj);
+})
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+app.get('/verify/:age', function(req, res) {
+  if (req.params.age > 13) {
+    res.sendStatus(200)
+  } else {
+    res.sendStatus(403)
+  }
+})
 
 app.use(function(req, res) {
   res.sendStatus(404);
